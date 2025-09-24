a13c3bc1e421199e0d7cbc172802f9a6d482643c
poteko <poteko@example.com>
2025-09-24 19:07:03 +0900

    station6

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 // DO NOT DELETE
-
+import { useState } from 'react'
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg'
+  )
+
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header>
+        <h1>Dogアプリ</h1>
+      </header>
+      <img src={dogUrl} alt="dog" width={200} />
+      <p>かわいい犬の画像です。</p>
+      <button
+        onClick={async () => {
+          const res = await fetch('https://dog.ceo/api/breeds/image/random')
+          const data = await res.json()
+          setDogUrl(data.message) 
+        }}
+      >
+        更新
+      </button>
     </div>
   )
 }
